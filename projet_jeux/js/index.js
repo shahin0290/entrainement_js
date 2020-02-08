a5a4922ee812b8b6c2ed4e690d06e01cdd6b5a06
shahin321 <shahinchoudhurypro@gmail.com>
2020-02-08 22:41:55 +0100

    Add 11th js slice method #5

--- projet_jeux/js/index.js
+++ projet_jeux/js/index.js
@@ -70,3 +70,8 @@ function myFunction1(a, b) {
 function displayDate() {
     document.getElementById("date").innerHTML = Date();
   }
+
+//Slice Methode slice()
+var str = "Apple, Banana, Kiwi";
+var res = str.slice(7,13);
+document.getElementById("slicemethod").innerHTML = res; 
