d63bfa1881deced42c2c4c8f77a92deefe8d99a4
shahin321 <shahinchoudhurypro@gmail.com>
2020-02-08 20:11:35 +0100

    Add 4th js variable #5

--- projet_jeux/js/index.js
+++ projet_jeux/js/index.js
@@ -1,1 +1,8 @@
 document.getElementById("demotest").innerHTML = "Hello Dolly.";
+
+
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+document.getElementById("demoVariable").innerHTML =
+"The total is: " + total;
