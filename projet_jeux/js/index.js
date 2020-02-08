9ee8bfe7cc3412cd15c7cef1e1dd4f405f7b3bd4
shahin321 <shahinchoudhurypro@gmail.com>
2020-02-08 20:17:26 +0100

    Add link index.js to all pages #5

--- projet_jeux/js/index.js
+++ projet_jeux/js/index.js
@@ -1,8 +1,12 @@
+
+//Statement 
 document.getElementById("demotest").innerHTML = "Hello Dolly.";
 
-
+//Varable
 var price1 = 5;
 var price2 = 6;
 var total = price1 + price2;
 document.getElementById("demoVariable").innerHTML =
 "The total is: " + total;
+
+
