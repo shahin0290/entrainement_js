e33557ab9cf750a05a8cd527b649762074f4b3b4
shahin321 <shahinchoudhurypro@gmail.com>
2020-02-11 00:01:27 +0100

    Add 41th Input validation exemple error function #5

--- projet_jeux/js/index.js
+++ projet_jeux/js/index.js
@@ -348,3 +348,21 @@ try {
   catch(err) {
     document.getElementById("error").innerHTML = err.message;
   }
+
+//Input validation exemple
+function myErrorFunction() {
+    var message, x;
+    message = document.getElementById("p01");
+    message.innerHTML = "";
+    x = document.getElementById("demoerror").value;
+    try { 
+      if(x == "")  throw "empty";
+      if(isNaN(x)) throw "not a number";
+      x = Number(x);
+      if(x < 5)  throw "too low";
+      if(x > 10)   throw "too high";
+    }
+    catch(err) {
+      message.innerHTML = "Input is " + err;
+    }
+  }
