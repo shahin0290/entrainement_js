3848732629816c7acc47f6ebb12d05857c7814bf
shahin321 <shahin321@hotmail.fr>
2021-01-05 22:19:48 +0100

    Add function 2 page1 #6

--- projet_jeux/js/index.js
+++ projet_jeux/js/index.js
@@ -31,8 +31,7 @@ document.getElementById("demotest").innerHTML = "Hello Dolly.";
 var price1 = 5;
 var price2 = 6;
 var total = price1 + price2;
-document.getElementById("demoVariable").innerHTML =
-"The total is: " + total;
+document.getElementById("demoVariable").innerHTML = "The total is: " + total;
 
 //Variable2
 var carName = "Volvo";
